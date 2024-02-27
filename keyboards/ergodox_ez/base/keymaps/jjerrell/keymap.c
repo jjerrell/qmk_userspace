22f9bcf9813279e7de76fb1c22a2fe6f6cb9a76e
Jacob Jerrell <jacob@jjerrell.dev>
2024-02-26 22:31:18 -0600

    Adds the current ergodox layout interpretation and memoire

--- keyboards/ergodox_ez/base/keymaps/jjerrell/keymap.c
+++ keyboards/ergodox_ez/base/keymaps/jjerrell/keymap.c
@@ -1,11 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
-
-enum layers {
-    BASE,  // default layer
-    SYMB,  // symbols
-    MDIA,  // media keys
-};
+#include "layouts.h"
 
 enum custom_keycodes {
     VRSN = SAFE_RANGE,
@@ -13,107 +8,73 @@ enum custom_keycodes {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
- */
-[BASE] = LAYOUT_ergodox_pretty(
-  // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
-  KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),                  MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT,
-  LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
-                                                           ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
-                                                                          KC_HOME,                 KC_PGUP,
-                                                         KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_TAB, KC_ENT
-),
-/* Keymap 1: Symbol Layer
- *
- * ,---------------------------------------------------.           ,--------------------------------------------------.
- * |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
- * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
- * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
- * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
- * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | EPRM  |      |      |      |      |                                       |      |    . |   0  |   =  |      |
- *   `-----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |Animat|      |       |Toggle|Solid |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |Bright|Bright|      |       |      |Hue-  |Hue+  |
- *                                 |ness- |ness+ |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-[SYMB] = LAYOUT_ergodox_pretty(
-  // left hand
-  VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,     KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-  KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,               KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-  KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,     KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-  EE_CLR,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-                                               RGB_MOD, KC_TRNS,     RGB_TOG, RGB_M_P,
-                                                        KC_TRNS,     KC_TRNS,
-                                      RGB_VAD, RGB_VAI, KC_TRNS,     KC_TRNS, RGB_HUD, RGB_HUI
-),
-/* Keymap 2: Media and mouse keys
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |  Play  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |Brwser|
- *                                 |      |      |------|       |------|      |Back  |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-[MDIA] = LAYOUT_ergodox_pretty(
-  // left hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,                                         KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
-
-                                               KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
-                                                        KC_TRNS,     KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_WBAK
-),
+    /* Keymap 0: Basic layer
+    *
+    * ,--------------------------------------------------.           ,--------------------------------------------------.
+    * | Esc    |   1  |   2  |   3  |   4  |   5  |  ->  |           |  -   |   6  |   7  |   8  |   9  |   0  |   =    |
+    * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+    * | Tab    |   Q  |   D  |   R  |   W  |   B  |  (   |           |  )   |   J  |   F  |   U  |   P  |   ;  |   \    |
+    * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+    * | CapWrd |   A  |S/Shft|H/Cmd |T/Alt |   G  |------|           |------|   Y  | N/Alt| E/Cmd|O/Shft|   I  |   '    |
+    * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
+    * | LShift |Z/Ctrl|   X  |   M  |   C  |   V  |      |           |      |   K  |   L  |   ,  |   .  |//Ctrl| RShift |
+    * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+    *   | Lead |      |      | Up   | Left |                                       | Right| Down |      |      | GAME |
+    *   `----------------------------------'                                       `----------------------------------'
+    *                                        ,-------------.       ,-------------.
+    *                                        |      |      |       |      |      |
+    *                                 ,------|------|------|       |------+--------+------.
+    *                                 |      |      |      |       |      |        |      |
+    *                                 |Space |BckSpc|------|       |------|  Tab   | Enter|
+    *                                 |      |      |RAISE |       | LOWER|        |      |
+    *                                 `--------------------'       `----------------------'
+    */
+    [_WORKMAN] = KEYMAP_ergodox_modifiers(
+        KC_ESC,  _________________NUMBERS_L_________________, KC_ARROW,    KC_MINS, _________________NUMBERS_R_________________, KC_EQL,
+        KC_TAB,  _________________WORKMN_L1_________________, KC_LPRN,     KC_RPRN, _________________WORKMN_R1_________________, KC_BSLS,
+        CW_TOGG, _________________WORKMN_L2_________________,                       _________________WORKMN_R2_________________, KC_QUOT,
+        KC_LSFT, _________________WORKMN_L3_________________, KC_LBRC,     KC_RBRC, _________________WORKMN_R3_________________, KC_RSFT,
+        QK_LEAD, XXXXXXX, XXXXXXX, KC_UP, KC_LEFT,                                          KC_RIGHT, KC_DOWN, XXXXXXX, XXXXXXX, KC_GAME,
+                                                    XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX,
+                                                            XXXXXXX,     XXXXXXX, 
+                                            KC_SPC, KC_BSPC, KC_RISE,     KC_LOWR, KC_TAB, KC_ENTER
+    ),
+    [_LOWER] = KEYMAP_ergodox_modifiers(
+        KC_HYPR, _______________________FROW_L_______________________,     _______________________FROW_R_______________________, KC_MEH,
+        _______, _________________LOWER_L1__________________, _______,     _______, _________________LOWER_R1__________________, _______,
+        _______, _________________LOWER_L2__________________,                       _________________LOWER_R2__________________, _______,
+        _______, _________________LOWER_L3__________________, _______,     _______, _________________LOWER_R3__________________, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                             KC_0, KC_DOT, KC_COMM, KC_PLUS, _______,
+                                                     _______, _______,     _______, _______,
+                                                              _______,     _______, 
+                                            _______, _______, _______,     _______, _______, _______
+    ),
+    [_RAISE] = KEYMAP_ergodox_modifiers(
+        _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+        _______, _________________RAISE_L1__________________, _______,     _______, _________________RAISE_R1__________________, _______,
+        _______, _________________RAISE_L2__________________,                       _________________RAISE_R2__________________, _______,
+        _______, _________________RAISE_L3__________________, _______,     _______, _________________RAISE_R3__________________, _______,
+        _______, _______, _______, _______, _______,                                         _______, _______, _______, _______, _______,
+                                                     _______, _______,     _______, _______,
+                                                              _______,     _______, 
+                                            _______, _______, _______,     _______, _______, _______
+    ),
+    /* Adjust (Lower + Raise) */
+    [_ADJUST] = KEYMAP_ergodox_layers(
+        _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+        _______, _________________ADJUST_L1_________________, _______,     _______, _________________ADJUST_R1_________________, _______,
+        _______, _________________ADJUST_L2_________________,                       _________________ADJUST_R2_________________, _______,
+           VRSN, _________________ADJUST_L3_________________, _______,     _______, _________________ADJUST_R3_________________, _______,
+        _______, _______, _______, _______, _______,                                         _______, _______, _______, _______, _______,
+                                                     _______, _______,     _______, _______,
+                                                              _______,     _______, 
+                                            _______, _______, _______,     _______, _______, _______
+    )
 };
+
 // clang-format on
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
             case VRSN:
@@ -132,11 +93,12 @@ void keyboard_post_init_user(void) {
 };
 
 // Runs whenever there is a layer state change.
-layer_state_t layer_state_set_user(layer_state_t state) {
+layer_state_t layer_state_set_keymap(layer_state_t state) {
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
+    ergodox_led_all_set(LED_BRIGHTNESS_DEFAULT);
 
     uint8_t layer = get_highest_layer(state);
     switch (layer) {
@@ -197,4 +159,51 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     }
 
     return state;
+};
+
+/* 
+    Setup to light up a combination of the 3 leds to indicate which modifiers are active.
+    Note that if a layer change occurs, this will not override any changes from the resulting layer.
+*/
+void matrix_scan_keymap(void) {
+    uint8_t modifiers = get_mods();
+    uint8_t layer_is_default = layer_state_is(_WORKMAN) || layer_state_is(_QWERTY);
+
+    if (modifiers && layer_is_default) {
+        if (modifiers & MOD_MASK_CSAG) {
+            // All modifiers are set
+            ergodox_led_all_on();
+            // Double LED 2's brightness since both control and command/win modifiers are active
+            ergodox_right_led_2_set(LED_BRIGHTNESS_DEFAULT * 2);
+        } else {
+            // Individual if/else blocks so the lights shut off if multiple mods are registered and one is released
+            if (modifiers & MOD_MASK_SHIFT) {
+                ergodox_right_led_1_on();
+            } else {
+                ergodox_right_led_1_off();
+            }
+
+            if (modifiers & MOD_MASK_ALT) {
+                ergodox_right_led_3_on();
+            } else {
+                ergodox_right_led_3_off();
+            }
+
+            if ((modifiers & MOD_MASK_CTRL) && (modifiers & MOD_MASK_GUI)) {
+                // Control and CMD share a light. If both are held, the brightness is doubled
+                ergodox_right_led_2_set(LED_BRIGHTNESS_DEFAULT * 2);
+                ergodox_right_led_2_on();
+            } else if ((modifiers & MOD_MASK_CTRL) || (modifiers & MOD_MASK_GUI)) {
+                // Only one of Control and CMD are active, use default brightness
+                ergodox_right_led_2_set(LED_BRIGHTNESS_DEFAULT);
+                ergodox_right_led_2_on();
+            } else {
+                ergodox_right_led_2_off();
+            }
+        }
+    } else if (layer_is_default) {
+        // reset brightness and shutoff LEDs unless we're on an alternate layer
+        ergodox_led_all_set(LED_BRIGHTNESS_DEFAULT);
+        ergodox_led_all_off();
+    }
 };
