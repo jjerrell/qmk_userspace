1ec0e4da79a05e2f7d684232eff3852a00674cce
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2025-06-08 18:55:46 -0500

    [Moonlander] Keymap updates

--- keyboards/zsa/moonlander/keymaps/techworker/keymap.h
+++ keyboards/zsa/moonlander/keymaps/techworker/keymap.h
@@ -15,13 +15,13 @@
     K21, K22, K23, K24, K25, K26,           K27, K28, K29, K2A, K2B, K2C, \
     K31, K32, K33, K34, K35, K36,           K37, K38, K39, K3A, K3B, K3C  \
 ) \
-    LAYOUT_moonlander_wrapper(                                                                                                                                    \
-        K01,      K02,      K03,      K04,      K05, K06,              KC_ARRW,          KC_MINS, K07,               K08,      K09,      K0A,      K0B,      K0C, \
-        K11,      K12,      K13,      K14,      K15, K16,              KC_LPRN,          KC_RPRN, K17,               K18,      K19,      K1A,      K1B,      K1C, \
-        K21,      K22,      K23,      K24,      K25, K26,              KC_LBRC,          KC_RBRC, K27,               K28,      K29,      K2A,      K2B,      K2C, \
-        K31,      K32,      K33,      K34,      K35, K36,                                         K37,               K38,      K39,      K3A,      K3B,      K3C, \
-        _______________HOME_L_EXTRA________________,                   _______,          _______,                    _______________HOME_R_EXTRA________________, \
-                                                     ____THUMBS_L____, QK_LEAD,          _______,   ____THUMBS_R____                                              \
+    LAYOUT_moonlander_wrapper(                                                                                                                                  \
+        K01,     K02,     K03,     K04,     K05,     K06,     KC_ARRW,                            KC_MINS,    K07,     K08,     K09,     K0A,     K0B,     K0C, \
+        K11,     K12,     K13,     K14,     K15,     K16,     KC_LPRN,                            KC_RPRN,    K17,     K18,     K19,     K1A,     K1B,     K1C, \
+        K21,     K22,     K23,     K24,     K25,     K26,     KC_LBRC,                            KC_RBRC,    K27,     K28,     K29,     K2A,     K2B,     K2C, \
+        K31,     K32,     K33,     K34,     K35,     K36,                                                     K37,     K38,     K39,     K3A,     K3B,     K3C, \
+        TOG_LWR, _______, _______, _______, QK_LEAD,                   _______,          _______,                  KC_SEC1, _______, _______, _______, TOG_RSE, \
+                                                     ____THUMBS_L____, _______,          _______, ____THUMBS_R____                                              \
     )
 // clang-format on
 
--- keyboards/zsa/moonlander/keymaps/techworker/rules.mk
+++ keyboards/zsa/moonlander/keymaps/techworker/rules.mk
@@ -1,12 +1,14 @@
 SWAP_HANDS_ENABLE     = yes
-AUTOCORRECT_ENABLE    = yes
+AUTOCORRECT_ENABLE    = no
 CAPS_WORD_ENABLE      = yes
-TAP_DANCE_ENABLE      = no
+TAP_DANCE_ENABLE      = yes
 BOOTMAGIC_ENABLE      = yes      # Enable Bootmagic Lite
+CUSTOM_UNICODE_ENABLE = yes
 
-# BOOTLOADER = tinyuf2
+BOOTLOADER = tinyuf2
 
 RGBLIGHT_ENABLE = yes
 RGBLIGHT_DRIVER = custom
 
+LTO_ENABLE = yes
 OPT = fast
