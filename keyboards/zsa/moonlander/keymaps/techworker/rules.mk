4f0a766901156bb4c2c104c858fb5f85ac8abf4f
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2025-07-27 21:40:19 -0500

    [Moonlander] New RGB Configuration

--- keyboards/zsa/moonlander/keymaps/techworker/rules.mk
+++ keyboards/zsa/moonlander/keymaps/techworker/rules.mk
@@ -1,11 +1,10 @@
-SWAP_HANDS_ENABLE     = yes
 AUTOCORRECT_ENABLE    = yes
 CAPS_WORD_ENABLE      = yes
+SWAP_HANDS_ENABLE     = no
 TAP_DANCE_ENABLE      = no
+
 BOOTMAGIC_ENABLE      = yes      # Enable Bootmagic Lite
 CUSTOM_UNICODE_ENABLE = yes
-
-# BOOTLOADER = tinyuf2 # Not working for this config (probably some Drashna stuff)
 
 # Enable RGB Matrix and RGB Light (indicators)
 # RGB_MATRIX_ENABLE = yes
